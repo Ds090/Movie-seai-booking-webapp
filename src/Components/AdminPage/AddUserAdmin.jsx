068d96bf6e4a22d409de493d8cb1db1ba71a97ea
Divanshu Kachhawa <divanshukachawa7878@gmail.com>
2024-06-23 17:56:20 +0530

    add

--- src/Components/AdminPage/AddUserAdmin.jsx
+++ src/Components/AdminPage/AddUserAdmin.jsx
@@ -25,8 +25,7 @@ export default function AddUserAdmin() {
             <thead className="bg-[#1a1a1a] text-white h-[50px]">
               <tr className="">
                 <th className="">USER NAME</th>
-                <th className="">TOTAL SEAT</th>
-                <th className="">TOTAL PAY AMOUNT</th>
+                <th className="">PASSWORD</th>
                 {/* <th className="border border-[#dddddd]">TIME</th> */}
                 <th className="">
                   <button className="bg-black hover:bg-stone-700 py-1 px-2 rounded text-white text-[14px]" onClick={handlePop}>
